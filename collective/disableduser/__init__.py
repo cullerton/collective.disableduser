266895ea153804b87cb53e2cdc2d8ddbb9e3e7d3
Mike Cullerton <webstuff@bakednotfried.com>
2014-08-11 08:49:52 -0600

    __init__ and TODO

--- collective/disableduser/__init__.py
+++ collective/disableduser/__init__.py
@@ -1,5 +1,15 @@
-# -*- extra stuff goes here -*-
+from Products.PluggableAuthService.PluggableAuthService \
+    import registerMultiPlugin
+from collective.disableduser import plugin
+from zope.i18nmessageid import MessageFactory
+
+appMessageFactory = MessageFactory('codec.app')
 
 
 def initialize(context):
     """Initializer called when used as a Zope 2 product."""
+    registerMultiPlugin(plugin.DisabledUserPlugin.meta_type)  # Add to PAS menu
+    context.registerClass(plugin.DisabledUserPlugin,
+                          constructors=(plugin.manage_addDisabledUserPluginForm,
+                          plugin.addDisabledUserPlugin),
+                          visibility=None)
